41600cd6680f68a54c61aa33afb9cfecee6b4447
Brogun Tibbitts <brogt3@gmail.com>
2022-09-22 10:53:14 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> names = new List<string>();
+        names.Add("Bob");
+        names.Add("Jimbo");
+        names.Add("Henrieta");
+
+        names.ForEach(name => Console.Write(name + " "));
+
+        List<string> stuff = new List<string>{"a", "b", "c", "d"};
+        stuff.ForEach(thing => Console.Write(thing + " "));
+
+        int x = 5;
+        int y = 7;
+        int result = adder(x,y);
+
+        Console.WriteLine($"{x} + {y} = {result}");
+
+    }
+
+    static int adder(int a, int b)
+    {
+        return a + b;
+    }
+}
